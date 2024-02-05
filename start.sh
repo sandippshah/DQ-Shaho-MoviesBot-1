89267dbec60986e5c0eba96c1bb2c36b833747ca
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-02-05 17:38:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sandippshah/DQ-Shaho-MoviesBot-1.git /DQShahoMoviesBot1
+  git clone git@github.com:sandippshah/DQ-Shaho-MoviesBot-1.git /DQShahoMoviesBot1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQShahoMoviesBot1

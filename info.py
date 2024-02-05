4348f68f4af1f4b7a808e79335ece3767f2ca5fd
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-02-05 17:52:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '622730585 1003337276').split()]
-BOT_OWNER_U = environ.get('BOT_OWNER_U, 'https://t.me/shaho007')
+BOT_OWNER_U = environ.get('BOT_OWNER_U', 'https://t.me/shaho007')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002140395533').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '622730585 1003337276').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
